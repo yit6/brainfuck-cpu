64e7307e91d6cd4cd5232f9f498031c4cd3d5154
yit6 <thatsjustwyatt@gmail.com>
2025-03-23 12:51:13 -0400

    move bracket search logic to control unit

--- vhdl/control.vhd
+++ vhdl/control.vhd
@@ -2,20 +2,40 @@ library ieee;
 
 use ieee.std_logic_1164.all;
 
-entity control is port
-	(
+entity control is
+	generic (
+		BIT_W : integer;
+		BRACKET_DEPTH : integer
+	);
+	port (
 		ins : in std_logic_vector(15 downto 0);
 		mode : in std_logic_vector(1 downto 0);
+		bracket_count : in std_logic_vector(BRACKET_DEPTH-1 downto 0);
+		mem_out : in std_logic_vector(BIT_W-1 downto 0);
+
 		inc_dec_src : out std_logic;
 		data_src    : out std_logic_vector(1 downto 0);
 		write_mem   : out std_logic;
 		write_addr  : out std_logic;
-		out_en      : out std_logic
+		out_en      : out std_logic;
+
+		next_mode : out std_logic_vector(1 downto 0);
+		addr_scan_back : out std_logic;
+		next_bracket_op : out std_logic_vector(1 downto 0)
 	);
 end entity control;
 
 architecture buh of control is
 	signal load_imm : std_logic;
+	
+	signal scan_open   : std_logic;
+	signal scan_close  : std_logic;
+	signal normal_mode : std_logic;
+
+	signal ins_open   : std_logic;
+	signal ins_close  : std_logic;
+
+	signal match : std_logic;
 begin 
 
 	load_imm <= and(ins(15 downto 8));
@@ -26,10 +46,32 @@ begin
 	data_src <= (ins(14) or ins(12)) & (ins(15) or ins(13));
 
 	-- write memory for + - ,
-	write_mem <= (and mode) and (ins(13) or ins(12) or ins(10));
+	write_mem <= normal_mode and (ins(13) or ins(12) or ins(10));
 
 	-- change address for > and <, but not load immediate
-	write_addr <= (and mode) and (ins(15) or ins(14)) and not load_imm;
+	write_addr <= normal_mode and (ins(15) or ins(14)) and not load_imm;
 
 	out_en <= ins(11) and not load_imm;
+
+	scan_open  <= '1' when mode="01" else '0';
+	scan_close <= '1' when mode="10" else '0';
+	normal_mode <= and mode;
+
+	ins_open  <= '1' when ins(15 downto 8)="00000010" else '0';
+	ins_close <= '1' when ins(15 downto 8)="00000001" else '0';
+
+	match <= '0' when (or bracket_count) else
+		 '1' when scan_open and ins_open else
+		 '1' when scan_close and ins_close else '0';
+
+	next_mode <= "11" when match else
+		     "10" when ins_open and not (or mem_out) else
+		     "01" when ins_close and (or mem_out) else mode;
+
+	addr_scan_back <= (normal_mode and ins_close and (or mem_out)) or (scan_open and not match);
+
+	next_bracket_op <= "00" when match else
+			   "10" when (scan_open and ins_close) or (scan_close and ins_open) else
+			   "01" when (scan_open and ins_open) or (scan_close and ins_close) else "11";
+
 end architecture buh;
--- vhdl/cpu.vhd
+++ vhdl/cpu.vhd
@@ -39,15 +39,26 @@ architecture buh of cpu is
 		);
 	end component execute;
 
-	component control is port
-		(
+	component control is 
+		generic (
+			BIT_W : integer;
+			BRACKET_DEPTH : integer
+		);
+		port (
 			ins : in std_logic_vector(15 downto 0);
 			mode : in std_logic_vector(1 downto 0);
+			bracket_count : in std_logic_vector(BRACKET_DEPTH-1 downto 0);
+			mem_out : in std_logic_vector(BIT_W-1 downto 0);
+
 			inc_dec_src : out std_logic;
 			data_src    : out std_logic_vector(1 downto 0);
 			write_mem   : out std_logic;
 			write_addr  : out std_logic;
-			out_en      : out std_logic
+			out_en      : out std_logic;
+
+			next_mode : out std_logic_vector(1 downto 0);
+			addr_scan_back : out std_logic;
+			next_bracket_op : out std_logic_vector(1 downto 0)
 		);
 	end component control;
 
@@ -86,6 +97,13 @@ architecture buh of cpu is
 	signal data_src    : std_logic_vector(1 downto 0);
 	signal write_mem   : std_logic;
 	signal write_addr  : std_logic;
+
+	signal match : std_logic;
+	signal next_mode : std_logic_vector(1 downto 0);
+	signal next_bracket_count : std_logic_vector(BRACKET_DEPTH-1 downto 0);
+	signal next_ins_address : std_logic_vector(PROG_W-1 downto 0);
+	signal next_backet_op : std_logic_vector(1 downto 0);
+	signal addr_scan_back : std_logic;
 begin
 	prog : program
 	generic map (PROG_W => PROG_W)
@@ -94,14 +112,21 @@ begin
 		ins => ins
 	);
 
-	con : control port map (
+	con : control
+	generic map (BRACKET_DEPTH => BRACKET_DEPTH, BIT_W => 8)
+	port map (
 		ins => ins,
 		mode => mode,
+		mem_out => char_out,
+		bracket_count => bracket_count,
 		inc_dec_src => inc_dec_src,
 		data_src    => data_src,
 		write_mem   => write_mem,
 		write_addr  => write_addr,
-		out_en      => out_en
+		out_en      => out_en,
+		next_mode   => next_mode,
+		next_bracket_op => next_backet_op,
+		addr_scan_back => addr_scan_back
 	);
 
 	ex : execute port map (
@@ -139,49 +164,23 @@ begin
 		y => dec_bracket_count
 	);
 
-	process(clk) is 
-		variable match : std_logic;
-	begin
-		if clk'event and clk='1' then
-			if mode="01" and ins(15 downto 8)="00000010" and (or bracket_count)='0' then
-				match := '1';
-			elsif mode="10" and ins(15 downto 8)="00000001" and (or bracket_count)='0' then
-				match := '1';
-			else
-				match := '0';
-			end if;
+	match <= '0' when (or bracket_count)='1' else
+		 '1' when mode="01" and ins(15 downto 8)="00000010" else
+		 '1' when mode="10" and ins(15 downto 8)="00000001" else '0';
 
-			if mode="11" then
-				if ins(15 downto 8)="00000010" and (or char_out)='0' then
-					mode <= "10";
-					addr <= next_addr;
-				elsif ins(15 downto 8)="00000001" and (or char_out)='1' then
-					mode <= "01";
-					addr <= prev_addr;
-				else
-					addr <= next_addr;
-				end if;
-			elsif match='1' then
-				bracket_count <= (others => '0');
-				addr <= next_addr;
-				mode <= "11";
-			else
-				if mode="10" then
-					addr <= next_addr;
-					if ins(15 downto 8)="00000010" then
-						bracket_count <= inc_bracket_count;
-					elsif ins(15 downto 8)="00000001" then
-						bracket_count <= dec_bracket_count;
-					end if;
-				elsif mode="01" then
-					addr <= prev_addr;
-					if ins(15 downto 8)="00000001" then
-						bracket_count <= inc_bracket_count;
-					elsif ins(15 downto 8)="00000010" then
-						bracket_count <= dec_bracket_count;
-					end if;
-				end if;
-			end if;
+	next_ins_address <= prev_addr when addr_scan_back else next_addr;
+
+	with next_backet_op select next_bracket_count <=
+		(others => '0') when "00",
+		inc_bracket_count when "01",
+		dec_bracket_count when "10",
+		bracket_count when others;
+
+	process(clk) is begin
+		if clk'event and clk='1' then
+			addr <= next_ins_address;
+			bracket_count <= next_bracket_count;
+			mode <= next_mode;
 		end if;
 	end process;
 end architecture buh;
